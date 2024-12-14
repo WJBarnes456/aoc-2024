739362e29b378dba87812161ec93417eb68581db
Will Barnes <will@wjbarn.es>
2024-12-14 12:08:20 +0000

    [day13] Implement part 2
    This would have been so much faster if I'd noticed that the problem is sufficiently constrained already! Oh well.

--- day13/day13.py
+++ day13/day13.py
@@ -1,10 +1,13 @@
-# this is a linear optimisation problem
 # find integers (a,b) such that a * a_x + b * b_x = prize_x; a * a_y + b * b_y = prize_y; minimising 3a + b
-# I'm certain there's a nice algorithm that solves this directly but I don't think I've had to implement it before. it just reminds me of doing lagrangian multipliers at university.
-# I'll read about this once I've got the general skeleton set up
-
-# rather than considering all solutions, you simply want to find some number of presses of a and b such that 
-# then all you need to do is find a single solution, then optimise 3a + b by going as close to that crossover point as we can
+# we can substitute one of these variables out - let's make it all in terms of a
+# b = (prize_x - a * a_x) / b_x (which is an integer)
+# substitute into (2) 
+# a * a_y + b_y * (prize_x - a * a_x) / b_x = prize_y
+# a * a_y * b_x + b_y * (prize_x - a * a_x) = prize_y * b_x
+# a * a_y * b_x - a * a_x * b_y = prize_y * b_x - prize_x * b_y
+# a (a_y * b_x - a_x * b_y) = prize_y * b_x - prize_x * b_y
+# a = (prize_y * b_x - prize_x * b_y) / (a_y * b_x - a_x * b_y)
+# ...huh!? and this gives exactly the sample solutions... it's constrained enough that you don't need to do any searching at all! I was misled!
 
 import re
 import math
@@ -15,48 +18,28 @@ class Machine:
         self.b = b
         self.target = target
      
-    # as I said, I'm sure there's a nice algorithm for this, but let's solve it the basic way to start off with...
+    # we can solve this completely analytically...
     def minimum_tokens(self):
-        limit = 100
+        (a, b, target) = (self.a, self.b, self.target)
+        numerator = (target[1] * b[0] - target[0] * b[1])
+        denominator = (a[1] * b[0] - a[0] * b[1])
 
-        target = self.target
-        a = self.a
-        b = self.b
-         
-        # search the space for solutions
-        # the number of button presses are heavily constrained so we don't need to search the whole space, we can just iterate over possible numbers of presses for one of a or b, and figure the other out
-        # I'm picking to iterate over b presses basically arbitrarily 
-        best_solution = (math.inf, math.inf, math.inf)
-
-        # starting from the maximum press number, or the number of presses required for just b to saturate one of the values (min is correct as otherwise we'd overshoot)...
-        max_b_presses = min(target[0] // b[0], target[1] // b[1])
-        for b_presses in range(0, max_b_presses + 1):
-            remaining_x = target[0] - b_presses * b[0]
-            if remaining_x % a[0] != 0:
-                # doesn't evenly divide the remainder - bin it.
-                continue
-
-            a_presses = remaining_x // a[0]
-            remaining_y = target[1] - b_presses * b[1]
-            if remaining_y != a[1] * a_presses:
-                # not a real solution, bin it.
-                continue
-
-            # assert this is a real solution (bit paranoid but worth checking)
-            if target[0] != (a[0] * a_presses + b[0] * b_presses) or target[1] != (a[1] * a_presses + b[1] * b_presses):
-                raise Exception(f"Thought {(a_presses, b_presses)} was a solution to {self}")
-
-            # we found a real solution
-            score = 3 * a_presses + b_presses
-            if score < best_solution[2]:
-                best_solution = (a_presses, b_presses, score)
-
-        best_score = best_solution[2]
-        if best_score == math.inf:
-            print(f"Machine {self} thinks it's inf") 
+        if numerator % denominator != 0:
+            # no integer solution - i.e. not solvable
             return 0
 
-        return best_score
+        # assert this is now an integer number of a presses
+        a_presses = numerator // denominator
+
+        # now just turn this into a score
+        b_numerator = (target[0] - a_presses * a[0])
+
+        if b_numerator % b[0] != 0:
+            print(f"Thought {a_presses} is a solution but {b_numerator / b[0]} is non-integer")
+
+        b_presses = (target[0] - a_presses * a[0]) // b[0]
+        
+        return a_presses * 3 + b_presses 
     
     def __str__(self):
         return f"Button A: X+{self.a[0]}, Y+{self.a[1]}\nButton B: X+{self.b[0]}, Y+{self.b[1]}\nPrize: X={self.target[0]}, Y={self.target[1]}"
@@ -105,7 +88,6 @@ def part2(puzzle):
         new_machines.append(Machine(machine.a, machine.b, new_target))
     
     new_puzzle = Puzzle(new_machines)
-    print([str(m) for m in new_puzzle.machines])
     return Puzzle(new_machines).minimum_tokens()
 
 def main():
