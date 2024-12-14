0b25dcdf94e5976eba7bf59f14df0094f05deb99
Will Barnes <will@wjbarn.es>
2024-12-14 13:39:25 +0000

    [day14] Implement part 2
    I made the mistake of looking at someone say how they solved this, hence why my solution might be reminiscent of theirs.
    
    I'm glad I did that as my gut was telling me "oh it'll fill the space so you need to do some fancy common multiple bits", so getting to "oh just brute force it and see what comes out" was fine (and I learned a bit from seeing people say what their solutions were)
    
    basically I don't regret saving potentially a long time of frustration trying to figure out what the question is even asking me :p

--- day14/day14.py
+++ day14/day14.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 # Python's built-in mod can return negative values (e.g. -1 mod 2 is -1), which we don't want. so define our own instead
 def mod(a, b):
@@ -42,6 +43,73 @@ class Puzzle:
         for q in quadrants:
             score *= q
         return score
+    
+    # the problem definition is not very precise, but I think this "christmas tree" shape is when one particular row is fully occupied by robots
+    # the downside is that I'm not sure which row it's going to be 
+    # i.e. this is going to be the lowest time when that row is fully occupied
+    # the very first AoC in 2015 was also in the shape of a Christmas tree https://adventofcode.com/2015/
+    # the input has 500 robots in it, filling a 103 x 101 space with the shape of that Christmas tree..
+    # the example has 12 robots in a 7 x 11 space, which is just about enough to form a Christmas tree
+    # my thought is that it could look something like
+    #    x
+    #   xox
+    #  xooox
+    # xxxxxxx
+    #   | |
+    # i.e. width of a layer is 2*(steps from top) - 1
+    # this implies that for a 103 wide space, the tree is 52 layers tall
+    # and the number of robots required is 1 + (50 * 2) + 103 = 204 robots
+    # this can't be the case for this design, there are far too many robots
+    # perhaps it's instead stepped at twice that height..? but in any case I'm expecting there to be some number of lined up robots
+    # I implemented a dumb solution just to see what it'd look like and unexpectedly just found the solution, I'm glad I didn't make too many assumptions as the clever way would've not worked at all.
+    def print_positions_at_time(self, seconds):
+        robot_positions = sorted(r.position_after(seconds, self.size) for r in self.robots)
+
+        print(f"Positions at second {seconds}")
+        
+        robot_positions.reverse() 
+        next_robot = robot_positions.pop()
+        for x in range(0, self.size[0]):
+            line = ""
+            for y in range(0, self.size[1]):
+                count = 0
+                while next_robot == (x,y):
+                    count += 1
+                    try:
+                        next_robot = robot_positions.pop()
+                    except IndexError:
+                        next_robot = None
+                
+                if count == 0:
+                    line += '.'
+                else:
+                    line += str(count)
+            print(line)
+    
+    def deduce_christmas_tree_time(self):
+        for seconds in range(100000):
+            robot_positions = [r.position_after(seconds, self.size) for r in self.robots]
+
+            # we are specifically looking for a horizontal line as part of the christmas tree, so order by y first, then look for consecutive x
+            inverted_positions = sorted((r[1], r[0]) for r in robot_positions)
+
+            prev_pos = (math.inf, math.inf)
+            consecutive = 0
+            for pos in inverted_positions:
+                if pos[0] == prev_pos[0] and pos[1] == prev_pos[1] + 1:
+                    consecutive += 1
+                else:
+                    consecutive = 0
+                
+                if consecutive > 10:
+                    print("Found 10 consecutive, printing image")
+                    self.print_positions_at_time(seconds)
+                    return seconds
+                
+                prev_pos = pos
+        
+        return None
+
 
 robot_re = re.compile('p=(\d+),(\d+) v=(-?\d+),(-?\d+)')
 
@@ -51,7 +119,7 @@ def accept_input():
         try:
             line = input()
         except EOFError:
-            return Puzzle(robots, (101,103))
+            return Puzzle(robots, (101, 103))
 
         match = robot_re.match(line)
 
@@ -65,8 +133,13 @@ def accept_input():
 def part1(puzzle):
     return puzzle.safety_factor_after_time(100)
 
+def part2(puzzle):
+    return puzzle.deduce_christmas_tree_time()
+
+
 def main():
     puzzle = accept_input()
     print(f"Part 1: {part1(puzzle)}")
+    print(f"Part 2: {part2(puzzle)}")
 
 main()
