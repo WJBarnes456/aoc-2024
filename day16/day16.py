c5715a1fca862265d722d8a1251df613734182a1
Will Barnes <will@wjbarn.es>
2024-12-16 23:14:47 +0000

    [day16] Implement part 2
    I feel like I lucked out a bit with how I implemented part 1 in that it left space to implement part 2 pretty simply.

--- day16/day16.py
+++ day16/day16.py
@@ -73,21 +73,26 @@ class Map:
     #
     # start from the end, and iterate backwards - drawback, we have uncertainty about directions, benefit is that we always have the cheapest route from that point to the end, and the cheapest way onto the exit is always to walk directly onto it
     # ...actually, wait a second, we can just use our plain old friend Dijkstra for this!
-    def calculate_cheapest_path(self):
+    def calculate_cheapest_paths(self):
         # start by searching from the start position with score 0
-        visited = set()
+        visited = {}
 
         # we add all 4 positions here because the only time that considering a 180 degree turn makes sense is at the start position
         # otherwise it always leads us back where we came which is pointless
-        heap = [(0, self.start_pos, (1,0)), (1000, self.start_pos, (0,1)), (1000, self.start_pos, (0,-1)), (2000, self.start_pos, (-1,0))]
+        heap = [(1000, self.start_pos, (0,1), None), (1000, self.start_pos, (0,-1), None), (2000, self.start_pos, (-1,0), None)]
         heapify(heap)
 
-        # each node in the heap is the score to get there, the position we'd reach, and the (x,y) delta of which way we're facing
-        node = (0, self.start_pos, (1,0))
+        # each node in the heap is the score to get there, the position we'd reach, the (x,y) delta of which way we're facing, and the node we came from
+        node = (0, self.start_pos, (1,0), None) 
         while node[1] != self.end_pos:
-            score, position, delta = node
+            score, position, delta, prev_node = node
             if (position, delta) in visited:
-                # we got the cheapest route here already, ignore it
+                # we got the cheapest route here already, but if the score's the same then we want to track that
+                (prev_score, prev_nodes) = visited[(position, delta)]
+                if score == prev_score:
+                    prev_nodes.add(prev_node)
+                
+                # now just move on
                 node = heappop(heap)
                 continue
 
@@ -107,14 +112,20 @@ class Map:
                     continue
 
                 # it's at least worth considering. push it to the queue!
-                heappush(heap, (score + score_delta, next_position, this_delta))
+                heappush(heap, (score + score_delta, next_position, this_delta, (position, delta)))
             
             # we can now just say we visited here
             # if we need to get the route out, then we'd need to store the node that got us here somehow (I was thinking "we can just follow -delta" but it's not that simple since we might turn and move simultaneously)
-            print(f"Visited {node}")
-            visited.add((position, delta)) 
+            #print(f"Visited {node}")
+            prev_nodes = set()
+            prev_nodes.add(prev_node)
+            visited[(position, delta)] = (score, prev_nodes)
+        
+        # we found the shortest path to the end node!
+        # Edge case: there might actually be multiple nodes that are the cheapest predecessor to the end node
+        # for my input, that's not the case, so don't even consider it - we can just follow the cheapest predecessors all the way back from the one before the end node
 
-        return node[0]
+        return (node[0], visited, prev_node)
 
 # gets the position after moving delta from the start position
 def next_pos(position, delta):
@@ -141,10 +152,26 @@ def accept_input():
         lines.append(line)
 
 def part1(map):
-    return map.calculate_cheapest_path()
+    return map.calculate_cheapest_paths()[0]
 
 def part2(map):
-    raise NotImplementedError("Part 2 not implemented")
+    (score, lookback, prev_node) = map.calculate_cheapest_paths()
+
+    # now follow the path dictionary back to sum the number of tiles...
+    visited = set()
+    to_visit = [prev_node]
+
+    node = prev_node
+    while len(to_visit) > 0:
+        visited.add(node[0])
+        to_visit += list(lookback[node][1])
+        node = to_visit.pop()
+        while node is None:
+            node = to_visit.pop()
+    
+    # we didn't count the start (is a tile but no predecessor) or end (we didn't even consider its predecessors and assumed it only had one), so add 2
+    return len(visited) + 2
+
 
 def main():
     map = accept_input()
